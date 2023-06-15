305eeda0775e88abac49d11c6b19c6e262c85640
Pawuloon <pdembicki1@gmail.com>
2023-06-15 17:51:31 +0200

    update

---
+++ Stock.Server/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Stock.Shared.Models;
+
+namespace Stock.Server.Services;
+
+public interface IUserService
+{
+    Task<User> GetUser(int id);
+    Task<IEnumerable<User>> GetUsers();
+    Task AddUser(User user);
+    Task<User> UpdateUser(User user);
+    Task DeleteUser(int id);
+    
+}
---
+++ Stock.Server/Services/UserService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock.Server.Data;
+using Stock.Shared.Models;
+
+namespace Stock.Server.Services;
+
+public class UserService : IUserService
+{
+    private StockDbContext _context;
+
+    public UserService(StockDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            throw new NullReferenceException();
+        }
+        return user;
+    }
+
+    public async Task<IEnumerable<User>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        if (users == null)
+        {
+            throw new NullReferenceException();
+        }
+        return users;
+    }
+
+    public async Task AddUser(User user)
+    {
+        await _context.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<User> UpdateUser(User user)
+    {
+        _context.Update(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task DeleteUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+        else
+        {
+            throw new NullReferenceException();
+        }
+    }
+}
