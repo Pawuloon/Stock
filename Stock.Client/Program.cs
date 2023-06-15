f554f540f33968255daecd616afcbf49edb85dfa
Pal <s24328@pjwstk.edu.pl>
2023-06-15 21:10:10 +0200

    up

--- Stock.Client/Program.cs
+++ Stock.Client/Program.cs
@@ -4,51 +4,51 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Stock;
 using Stock.Data;
 
-namespace Stock
+// namespace Stock
+// {
+//     public class Program
+//     {
+//         public static async Task Main(string[] args)
+//         {
+//             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//             builder.RootComponents.Add<App>("#app");
+//
+//             builder.Services.AddScoped(sp =>
+//                 new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//
+//             await builder.Build().RunAsync();
+//         }
+//     }
+// }
+
+
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp =>
-                new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 
+app.UseRouting();
 
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
-// var builder = WebApplication.CreateBuilder(args);
-//
-// // Add services to the container.
-// builder.Services.AddRazorPages();
-// builder.Services.AddServerSideBlazor();
-// builder.Services.AddSingleton<WeatherForecastService>();
-//
-// var app = builder.Build();
-//
-// // Configure the HTTP request pipeline.
-// if (!app.Environment.IsDevelopment())
-// {
-//     app.UseExceptionHandler("/Error");
-//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//     app.UseHsts();
-// }
-//
-// app.UseHttpsRedirection();
-//
-// app.UseStaticFiles();
-//
-// app.UseRouting();
-//
-// app.MapBlazorHub();
-// app.MapFallbackToPage("/_Host");
-//
-// app.Run();
+app.Run();
