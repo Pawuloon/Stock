b8fb1776363bf5b2ad4c3ba123028e1e33ef25a8
Pawuloon <pdembicki1@gmail.com>
2023-06-15 13:26:04 +0200

    Models

---
+++ Stock.Shared/Models/PriceCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace Stock.Shared.Models;
+
+public class PriceCollection
+{
+    public int Id { get; set; }
+    public string? Ticker { get; set; }
+    public int? QueryCount { get; set; }
+    public string? Results { get; set; }
+    public int? Adjusted { get; set; }
+    public string? Status { get; set; }
+    public string? RequestId { get; set; }
+    public int? Count { get; set; }
+}
---
+++ Stock.Shared/Models/PriceData.cs
@@ -0,0 +1,16 @@
+﻿namespace Stock.Shared.Models;
+
+public class PriceData
+{
+    public int Id { get; set; }
+    public string? Symbol {get; set;}
+    public bool Status { get; set; }
+    public DateTime? From { get; set; }
+    public decimal? Open { get; set; }
+    public decimal? High { get; set; }
+    public decimal? Low { get; set; }
+    public decimal? Close { get; set; }
+    public int? Volume { get; set; }
+    public decimal? AfterHours { get; set; }
+    public decimal? PreMarket { get; set; }
+}
