e21007f9be1269340f3c649c87b09770253d84d2
Pawuloon <pdembicki1@gmail.com>
2023-06-17 14:49:18 +0200

    polygon added

---
+++ Stock.Server/Services/IPolygonService.cs
@@ -0,0 +1,8 @@
+﻿using Stock.Shared.Models;
+
+namespace Stock.Server.Services;
+
+public interface IPolygonService
+{
+    Task<Company> GetCompanyAsync(string symbol);
+}
---
+++ Stock.Server/Services/PolygonService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Stock.Shared.Models;
+
+namespace Stock.Server.Services;
+
+public class PolygonService : IPolygonService
+{
+
+    private const string BaseUrl = "https://api.polygon.io/v1/meta/symbols/";
+
+    public async Task<Company> GetCompanyAsync(string symbol)
+    {
+        var apiKey = "wLLws_fgOA0rv8cB_toGfvdBCpcd1yCj";
+        var url = $"{BaseUrl}{symbol}/company?apiKey={apiKey}";
+        using (HttpClient client = new())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode) 
+                throw new Exception($"Error getting company for {symbol}");
+            
+            var content = await response.Content.ReadAsStringAsync();
+            var company = JsonSerializer.Deserialize<Company>(content);
+            return company;
+        }
+    }
+}
